d452ed9ffa1659f77666b07689bf3b9b98bfcca3
Ruth Campion <97449315+RJCart@users.noreply.github.com>
2025-01-12 21:56:57 +0000

    Add files via upload

---
+++ Data_CA2/CA2/Shiny/app.R
@@ -0,0 +1,19 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+
+  ui <- fluidPage(
+    titlePanel("Linear Regression Map"),
+    mainPanel(
+      img(src = "C:/Users/Ruth/Desktop/Ca1_RuthC/Data_CA2/CA2/Linear Regression", height = "400px")
+    )
+  )
